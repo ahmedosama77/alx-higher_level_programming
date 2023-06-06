6f8db714b265499b237f177ec59e09e5de6364c9
= <=>
2023-06-06 07:21:29 -0700

    0. Positive anything is better than negative nothing

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("{}Is positive".format(number))
+    print("{} is positive".format(number))
 elif number == 0:
-    print("{}Is zero".format(number))
+    print("{} is zero".format(number))
 else:
-    print("{}Is negative".format(number))
+    print("{} is negative".format(number))
