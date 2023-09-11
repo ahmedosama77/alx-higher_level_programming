6fd59df4e6adbfb08df3003d71fcc12dc82c1c21
= <=>
2023-09-11 16:52:45 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-//  new function incr that increments the integer value.
-
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
-  myObject.value++;
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
@@ -15,4 +14,3 @@ myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
